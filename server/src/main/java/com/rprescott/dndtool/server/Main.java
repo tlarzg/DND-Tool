edb9d21ea34787a0caeb10a675205b68b3b2e2fd
Robert Prescott <rprescottjr@gmail.com>
2016-03-19 21:49:35 -0400

    Initial commit for the DND Server. It does literally nothing but start right now.

---
+++ server/src/main/java/com/rprescott/dndtool/server/Main.java
@@ -0,0 +1,12 @@
+package com.rprescott.dndtool.server;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("com/rprescott/dndtool/server/config/application-context.xml");
+        
+    }
+}
